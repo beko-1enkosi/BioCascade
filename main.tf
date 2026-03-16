c409703915692c0e1a10f0e9cd252c48d2dce63c
Thobeka Thobile Busisiwe <thnkojhb025@student.wethinkcode.com>
2026-03-16 09:34:16 +0200

    Finalized main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# 1. SETUP: Tell Terraform which plugins to use
 terraform {
   required_providers {
     coder = {
@@ -10,33 +9,30 @@ terraform {
   }
 }
 
-# 2. DATA: Get user and workspace info
 data "coder_workspace" "me" {}
 
-# 3. INTERFACE: The Interactive Parameter (The "Toggle")
+# INTERACTIVE INTERFACE: Using the guide's bool type for a toggle [cite: 13]
 data "coder_parameter" "gpu_enabled" {
   name         = "gpu_enabled"
   display_name = "Enable GPU Support?"
-  type         = "bool"
-  default      = "false"
-  order        = 1
+  type         = "bool"      # [cite: 13]
+  default      = "false"     # [cite: 32, 72]
+  order        = 1           # Places this at the top of the form [cite: 111, 113]
+  mutable      = true        # Allows users to toggle it on restart [cite: 139]
 }
 
-# 4. AGENT: The "Soul" of the workspace (where VS Code lives)
 resource "coder_agent" "main" {
   arch           = "amd64"
   os             = "linux"
-  # This matches the /workspace/app/app.py path we found in your Exec check!
+  # This matches your verified internal path /workspace/app/app.py
   startup_script = "python3 /workspace/app/app.py"
 
-  # Adds a button in the Coder UI to open your app
   display_apps {
     vscode          = true
     port_forward_6d = true
   }
 }
 
-# 5. IMAGE: Build your Dockerfile locally
 resource "docker_image" "biocascade_image" {
   name = "coder-biocascade"
   build {
@@ -44,21 +40,11 @@ resource "docker_image" "biocascade_image" {
   }
 }
 
-# 6. CONTAINER: The live workspace
 resource "docker_container" "workspace" {
   count = data.coder_workspace.me.start_count
   image = docker_image.biocascade_image.name
-  
-  # Uses the user's name to keep the container unique
   name  = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
   
-  # Connects the container to the Coder dashboard
   entrypoint = ["sh", "-c", replace(coder_agent.main.init_script, "/localhost/i", "localhost")]
   env        = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
-  
-  # Keeps the container from shutting down immediately
-  host {
-    host = "host.docker.internal"
-    ip   = "host-gateway"
-  }
 }
